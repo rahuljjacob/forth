d91b23c12f84b9b2bdcd1d144fecb50a117e1f97
Rahul <rahul.j.jacob@gmail.com>
2024-07-05 11:32:36 +0530

    some functionality

--- main.c
+++ main.c
@@ -1,15 +1,18 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 typedef struct node{
-	int data;
+	float data;
 	struct node* next;	
 }NODE;
 
 typedef struct stack{
 	NODE* top;
 }STACK;
+
+STACK* stack;
 
 void parse(char* token);
 void tokenize(char* str);
@@ -23,13 +26,61 @@ int peek(STACK* l);
 void printstack(STACK* l);
 
 int main(){
-	STACK* stack = init();
+	stack = init();
 	char str[100];
 	fgets(str, 100, stdin);
+	str[strcspn(str, "\n")] = 0;
 	while(1){
 		tokenize(str);
+		printf("\n");
 		fgets(str, 100, stdin);
+		str[strcspn(str, "\n")] = 0;
 	}
+}
+
+int isDefinedWord(char* token){
+	return 0;
+}
+
+int isPredefinedWord(char* token){
+	if (!strcmp(token, "+")){
+		push(stack, (pop(stack) + pop(stack)));
+		return 1;
+	}
+	else if(!strcmp(token, "-")){
+		push(stack, (pop(stack) - pop(stack)));
+		return 1;
+	}
+	return 0;
+}
+
+int isNumber(char* token){
+	for(int i = 0; i < strlen(token); i++){
+		if(!isdigit(token[i]))
+			return 0;
+	}
+	return 1;
+}
+
+void parse(char* token){
+	if(isDefinedWord(token)){
+	}
+	else if (isPredefinedWord(token)){
+	}
+	else if(isNumber(token)){
+		push(stack, atoi(token));
+	}
+	else 
+		printf(">> %s ?", token);
+}
+
+void tokenize(char* str){
+	char* token = strtok(str, " ");
+	while (token != NULL) {
+		parse(token);
+		token = strtok(NULL, " ");
+	}
+
 }
 
 NODE* createnode(int data){
@@ -73,11 +124,11 @@ int pop(STACK* l){
 void printstack(STACK* l){
 	NODE* printnode = l -> top;
 	while (printnode -> next != NULL){
-		printf("%d", printnode -> data);
+		printf("%f", printnode -> data);
 		printf("\n");
 		printnode = printnode -> next;
 	}
-	printf("%d \n", printnode -> data);
+	printf("%f \n", printnode -> data);
 }
 
 int peek(STACK* l){
@@ -87,17 +138,3 @@ int peek(STACK* l){
 	}
 	return l -> top -> data;
 }
-
-void parse(char* token){
-	
-}
-
-void tokenize(char* str){
-	char* token = strtok(str, " ");
-	while (token != NULL) {
-		parse(token);
-		token = strtok(NULL, " ");
-	}
-
-}
-
