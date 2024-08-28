053a88073b5e1c47d530fb97fe14c09c24b135cc
Rahul <rahul.j.jacob@gmail.com>
2024-08-28 11:06:07 +0530

    refactoring

--- main.c
+++ main.c
@@ -17,6 +17,10 @@ STACK* stack;
 void parse(char* token);
 void tokenize(char* str);
 
+int isDefinedWord(char* token);
+int isPredefinedWord(char* token);
+int isNumber(char* token);
+
 NODE* createnode(int data);
 int isEmpty(STACK* l);
 STACK* init();
@@ -25,6 +29,14 @@ int pop(STACK* l);
 int peek(STACK* l);
 void printstack(STACK* l);
 
+void add(STACK *l);
+void subtract(STACK *l);
+int dot(STACK *l);
+void dup(STACK *l);
+void drop(STACK *l);
+void over(STACK *l);
+void rot(STACK *l);
+
 int main(){
 	stack = init();
 	char str[100];
@@ -32,7 +44,6 @@ int main(){
 	str[strcspn(str, "\n")] = 0;
 	while(1){
 		tokenize(str);
-		printf("\n");
 		fgets(str, 100, stdin);
 		str[strcspn(str, "\n")] = 0;
 	}
@@ -44,15 +55,82 @@ int isDefinedWord(char* token){
 
 int isPredefinedWord(char* token){
 	if (!strcmp(token, "+")){
-		push(stack, (pop(stack) + pop(stack)));
+		add(stack);
 		return 1;
 	}
 	else if(!strcmp(token, "-")){
-		push(stack, (pop(stack) - pop(stack)));
+		subtract(stack);
 		return 1;
+	}
+	else if(!strcmp(token, ".")){
+		printf("%d \n", dot(stack));
+		return 1;
+	}
+	else if(!strcmp(token, "dup")){
+		dup(stack);
+		return 1;
+	}
+	else if(!strcmp(token, "drop")){
+		drop(stack);
+		return 1;
+	}
+	else if(!strcmp(token, "over")){
+		over(stack);
+	}
+	else if(!strcmp(token, "rot")){
+		rot(stack);
+	}
+	else if(!strcmp(token, "emit")){
+
+	}
+	else if(!strcmp(token, ".\"")){
+
+	}
+	else if(!strcmp(token, "=")){
+
+	}
+	else if(!strcmp(token, "variable")){
+
 	}
 	return 0;
 }
+
+void add(STACK *l){
+	push(stack, (pop(stack) + pop(stack)));
+}
+
+void subtract(STACK *l){
+	push(stack, (pop(stack) - pop(stack)));
+}
+
+int dot(STACK *l){
+	return pop(stack);
+}
+
+void dup(STACK *l){
+	push(stack, peek(stack));
+}
+
+void drop(STACK *l){
+	pop(stack);
+};
+
+void over(STACK *l){
+	int over1 = pop(stack);
+	int over2 = pop(stack);
+	push(stack, over2);
+	push(stack, over1);
+	push(stack, over2);
+};
+
+void rot(STACK *l){
+	int rot1 = pop(stack);
+	int rot2 = pop(stack);
+	int topElement = pop(stack);
+	push(stack, rot2);
+	push(stack, rot1);
+	push(stack, topElement);
+};
 
 int isNumber(char* token){
 	for(int i = 0; i < strlen(token); i++){
@@ -63,15 +141,13 @@ int isNumber(char* token){
 }
 
 void parse(char* token){
-	if(isDefinedWord(token)){
-	}
-	else if (isPredefinedWord(token)){
-	}
+	if(isDefinedWord(token));
+	else if (isPredefinedWord(token));
 	else if(isNumber(token)){
 		push(stack, atoi(token));
 	}
 	else 
-		printf(">> %s ?", token);
+		printf(">> %s? \n", token);
 }
 
 void tokenize(char* str){
