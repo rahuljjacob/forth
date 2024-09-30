4fd58c4f8d593877a70659bb187cdf8a74ee8663
Rahul <rahul.j.jacob@gmail.com>
2024-09-30 18:04:47 +0530

    heavy changes

--- main.c
+++ main.c
@@ -1,216 +1,213 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
-typedef struct node{
-	float data;
-	struct node* next;	
-}NODE;
+#define MAX_WORD_LENGTH 100
+#define DICTIONARY_SIZE 100
 
-typedef struct stack{
-	NODE* top;
-}STACK;
+typedef struct node {
+  float data;
+  struct node *next;
+} NODE;
 
-STACK* stack;
+typedef struct stack {
+  NODE *top;
+} STACK;
 
-void parse(char* token);
-void tokenize(char* str);
+typedef struct {
+  char name[MAX_WORD_LENGTH];
+  int type;
+  union {
+    void (*function)(STACK *);
+    char *definition;
+  } executable;
+} Word;
 
-int isDefinedWord(char* token);
-int isPredefinedWord(char* token);
-int isNumber(char* token);
+STACK *stack;
+Word dictionary[DICTIONARY_SIZE];
+int dictSize = 0;
+int wordMode = 0;
+int stringMode = 0;
 
-NODE* createnode(int data);
-int isEmpty(STACK* l);
-STACK* init();
-void push(STACK* l, int data);
-int pop(STACK* l);
-int peek(STACK* l);
-void printstack(STACK* l);
+int isWord(char *token);
+int isNumber(char *token);
 
+// Stack functions
+NODE *createnode(int data);
+int isEmpty(STACK *l);
+STACK *init();
+void push(STACK *l, int data);
+int pop(STACK *l);
+int peek(STACK *l);
+void printstack(STACK *l);
+
+// forth words
 void add(STACK *l);
 void subtract(STACK *l);
-int dot(STACK *l);
+void dot(STACK *l);
 void dup(STACK *l);
 void drop(STACK *l);
 void over(STACK *l);
 void rot(STACK *l);
+void cr(STACK *l);
+void emit(STACK *l);
 
-int main(){
-	stack = init();
-	char str[100];
-	fgets(str, 100, stdin);
-	str[strcspn(str, "\n")] = 0;
-	while(1){
-		tokenize(str);
-		fgets(str, 100, stdin);
-		str[strcspn(str, "\n")] = 0;
-	}
+void printString(char *str);
+void execWord(char *token);
+void addPredefinedWord(char *name, void (*function)(STACK *));
+void initDictionary();
+
+int main() {
+  char input[256];
+  char *token;
+
+  stack = init();
+  initDictionary();
+
+  while (1) {
+    printf("> ");
+    if (fgets(input, sizeof(input), stdin) == NULL) {
+      break;
+    }
+
+    token = strtok(input, " \t\n");
+    while (token != NULL) {
+      execWord(token);
+      token = strtok(NULL, " \t\n");
+    }
+  }
 }
 
-int isDefinedWord(char* token){
-	return 0;
+void addPredefinedWord(char *name, void (*function)(STACK *)) {
+  if (dictSize < DICTIONARY_SIZE) {
+    strcpy(dictionary[dictSize].name, name);
+    dictionary[dictSize].executable.function = function;
+    dictSize++;
+  } else {
+    printf("Dictionary Full");
+  }
 }
 
-int isPredefinedWord(char* token){
-	if (!strcmp(token, "+")){
-		add(stack);
-		return 1;
-	}
-	else if(!strcmp(token, "-")){
-		subtract(stack);
-		return 1;
-	}
-	else if(!strcmp(token, ".")){
-		printf("%d \n", dot(stack));
-		return 1;
-	}
-	else if(!strcmp(token, "dup")){
-		dup(stack);
-		return 1;
-	}
-	else if(!strcmp(token, "drop")){
-		drop(stack);
-		return 1;
-	}
-	else if(!strcmp(token, "over")){
-		over(stack);
-	}
-	else if(!strcmp(token, "rot")){
-		rot(stack);
-	}
-	else if(!strcmp(token, "emit")){
-
-	}
-	else if(!strcmp(token, ".\"")){
-
-	}
-	else if(!strcmp(token, "=")){
-
-	}
-	else if(!strcmp(token, "variable")){
-
-	}
-	return 0;
+void initDictionary() {
+  addPredefinedWord("+", add);
+  addPredefinedWord("-", subtract);
+  addPredefinedWord(".", dot);
+  addPredefinedWord("dup", dup);
+  addPredefinedWord("drop", drop);
+  addPredefinedWord("over", over);
+  addPredefinedWord("rot", rot);
+  addPredefinedWord("cr", cr);
+  addPredefinedWord("emit", emit);
 }
 
-void add(STACK *l){
-	push(stack, (pop(stack) + pop(stack)));
+void execWord(char *token) {
+  for (int i = 0; i < dictSize; i++) {
+    if (strcmp(token, dictionary[i].name) == 0) {
+      dictionary[i].executable.function(stack);
+      return;
+    }
+  }
+  if (isNumber(token)) {
+    push(stack, atoi(token));
+  }
 }
 
-void subtract(STACK *l){
-	push(stack, (pop(stack) - pop(stack)));
-}
+int isWord(char *token) {}
 
-int dot(STACK *l){
-	return pop(stack);
-}
+void printString(char *str) {}
 
-void dup(STACK *l){
-	push(stack, peek(stack));
-}
+void add(STACK *l) { push(stack, (pop(stack) + pop(stack))); }
 
-void drop(STACK *l){
-	pop(stack);
+void subtract(STACK *l) { push(stack, (pop(stack) - pop(stack))); }
+
+void dot(STACK *l) { printf("%d", pop(stack)); }
+
+void dup(STACK *l) { push(stack, peek(stack)); }
+
+void drop(STACK *l) { pop(stack); };
+
+void cr(STACK *l) { printf("\n"); }
+
+void emit(STACK *l) { printf("%d", pop(stack)); }
+
+void over(STACK *l) {
+  int over1 = pop(stack);
+  int over2 = pop(stack);
+  push(stack, over2);
+  push(stack, over1);
+  push(stack, over2);
 };
 
-void over(STACK *l){
-	int over1 = pop(stack);
-	int over2 = pop(stack);
-	push(stack, over2);
-	push(stack, over1);
-	push(stack, over2);
+void rot(STACK *l) {
+  int rot1 = pop(stack);
+  int rot2 = pop(stack);
+  int topElement = pop(stack);
+  push(stack, rot2);
+  push(stack, rot1);
+  push(stack, topElement);
 };
 
-void rot(STACK *l){
-	int rot1 = pop(stack);
-	int rot2 = pop(stack);
-	int topElement = pop(stack);
-	push(stack, rot2);
-	push(stack, rot1);
-	push(stack, topElement);
-};
-
-int isNumber(char* token){
-	for(int i = 0; i < strlen(token); i++){
-		if(!isdigit(token[i]))
-			return 0;
-	}
-	return 1;
+int isNumber(char *token) {
+  for (int i = 0; i < strlen(token); i++) {
+    if (!isdigit(token[i]))
+      return 0;
+  }
+  return 1;
 }
 
-void parse(char* token){
-	if(isDefinedWord(token));
-	else if (isPredefinedWord(token));
-	else if(isNumber(token)){
-		push(stack, atoi(token));
-	}
-	else 
-		printf(">> %s? \n", token);
+NODE *createnode(int data) {
+  NODE *newnode = (NODE *)malloc(sizeof(NODE));
+  newnode->next = NULL;
+  newnode->data = data;
+  return newnode;
 }
 
-void tokenize(char* str){
-	char* token = strtok(str, " ");
-	while (token != NULL) {
-		parse(token);
-		token = strtok(NULL, " ");
-	}
-
+int isEmpty(STACK *l) {
+  if (l->top == NULL)
+    return 1;
+  else
+    return 0;
 }
 
-NODE* createnode(int data){
-	NODE* newnode = (NODE*)malloc(sizeof(NODE));
-	newnode -> next = NULL;
-	newnode -> data = data;
-	return newnode;
+STACK *init() {
+  STACK *stack = (STACK *)malloc(sizeof(STACK));
+  stack->top = NULL;
+  return stack;
 }
 
-int isEmpty(STACK* l){
-	if (l -> top == NULL)
-		return 1;
-	else 
-		return 0;
+void push(STACK *l, int data) {
+  NODE *newhead = createnode(data);
+  newhead->next = l->top;
+  l->top = newhead;
 }
 
-STACK* init(){
-	STACK* stack = (STACK*)malloc(sizeof(STACK));
-	stack -> top = NULL;
-	return stack;
+int pop(STACK *l) {
+  if (isEmpty(l)) {
+    printf("stack underflow\n");
+    return -1;
+  }
+  NODE *oldhead = l->top;
+  int popped = oldhead->data;
+  l->top = l->top->next;
+  free(oldhead);
+  return popped;
 }
 
-void push(STACK* l, int data){
-	NODE* newhead = createnode(data);
-	newhead -> next = l -> top;
-	l -> top = newhead;
+void printstack(STACK *l) {
+  NODE *printnode = l->top;
+  while (printnode->next != NULL) {
+    printf("%f", printnode->data);
+    printf("\n");
+    printnode = printnode->next;
+  }
+  printf("%f \n", printnode->data);
 }
 
-int pop(STACK* l){
-	if(isEmpty(l)){
-		printf("stack underflow\n");
-		return -1;
-	}
-	NODE* oldhead = l -> top;
-	int popped = oldhead -> data;
-	l -> top = l -> top -> next;
-	free(oldhead);
-	return popped;
-}
-
-void printstack(STACK* l){
-	NODE* printnode = l -> top;
-	while (printnode -> next != NULL){
-		printf("%f", printnode -> data);
-		printf("\n");
-		printnode = printnode -> next;
-	}
-	printf("%f \n", printnode -> data);
-}
-
-int peek(STACK* l){
-	if (isEmpty(l)){
-		printf("Empty Stack");
-		return 0;
-	}
-	return l -> top -> data;
+int peek(STACK *l) {
+  if (isEmpty(l)) {
+    printf("Empty Stack");
+    return 0;
+  }
+  return l->top->data;
 }
